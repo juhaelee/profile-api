747be35fbc511f8dd7aef8dcd3a04da98a991f00
juhaelee <juhaelee@users.noreply.github.com>
2020-02-27 17:20:50 -0800

    testing

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  NAMESPACE_ID: "",
-  ACCESS_SECRET: ""
+  NAMESPACE_ID: "jmsZuild84",
+  ACCESS_SECRET: "sLXUuIJDcWesAPo9pLe2HzeLkEaGmjU-lwPpIsp6Is7XyM_wPjx9N1sGqqdaMAqrnsvbI2HLZSRkszBUMCcCkaV9Z8Pe3sgbfd2H6BfPnyU20MzCAhzNtcWCwh0pylg-f9Uv2xUdqJrA9geZ4vfUWsfAdmR7YNaQGTVR9f_vQle2X9FXdLbs0RFxyJRh5udPqFN7eXa_NUui"
 };
